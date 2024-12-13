e807d671e3b719389254b65738d4059fe5d724fa
ashil rahim <ashilrahim2@gmail.com>
2024-12-13 23:44:22 +0530

    signup updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
   const { fullname, email, password } = req.body;
   try {
@@ -22,11 +24,24 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
 
-    if(newUser) {
+    if (newUser) {
       // generate a jwt token here
-      
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error in the signup controller", error.message);
+    res.status(500).json({ message: "Internal Server Error " });
+  }
 };
 
 export const login = (req, res) => {
