80f75d6507c39ebe21796ab68313608fa51dd736
ashil rahim <ashilrahim2@gmail.com>
2024-12-13 23:33:40 +0530

    created a signup control

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,32 @@
-export const signup = (req, res) => {
-  
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+  const { fullname, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "user already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+
+    if(newUser) {
+      // generate a jwt token here
+      
+    }
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
@@ -7,5 +34,5 @@ export const login = (req, res) => {
 };
 
 export const logout = (req, res) => {
-    res.send("logout route");
-  }
+  res.send("logout route");
+};
