fd1c95e0030cd0506e4b9a00a858987fb8b237e4
ashil rahim <ashilrahim2@gmail.com>
2024-12-29 21:23:53 +0530

    minor changes

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,40 +1,96 @@
 import { useEffect } from "react";
-import { useChatStore } from "../store/useChatStore"
+import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
-
+import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 const ChatContainer = () => {
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    deleteMessage,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
 
-    const { messages,
-        getMessages,
-        isMessageLoading,
-        selectedUser } = useChatStore();
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
 
-    useEffect(() => {
-        getMessages(selectedUser._id);
-    }, [selectedUser._id, getMessages])
+  const handleDeleteMessage = (messageId) => {
+    deleteMessage(messageId);
+  };
 
-    if (isMessageLoading) {
-        return (
-          <div className="flex-1 flex flex-col overflow-auto">
-            <ChatHeader />
-            <MessageSkeleton />
-            <MessageInput />
-          </div>
-        );
-      }
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
-        <ChatHeader />
+      <ChatHeader />
 
-        <p>..messages</p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${
+              message.senderId === authUser._id ? "chat-end" : "chat-start"
+            } relative group`}
+          >
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col relative">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+              
+              {/* Positioned delete button based on message direction */}
+              {message.senderId === authUser._id && (
+                <button
+                  onClick={() => handleDeleteMessage(message._id)}
+                  className="absolute -right-2 top-0 hidden group-hover:block hover:bg-red-600 bg-red-500 text-white p-1 rounded-full transition-colors"
+                >
+                  <MdOutlineDeleteOutline size={16} />
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
 
-        <MessageInput />
+      <MessageInput />
     </div>
-  )
-}
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
