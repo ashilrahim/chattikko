f72d43851e665e8a5d1794f2bbe4a51f01bf1f74
ashil rahim <ashilrahim2@gmail.com>
2024-12-30 19:37:45 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,12 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
+
+
+
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
@@ -21,7 +24,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on port: " + PORT);
   connectDB();
 });
